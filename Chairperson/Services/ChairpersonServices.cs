46570b6a3f15a4affc43a115b33586f52eca8164
Ahsan Malik <m.ahsan.malik951@gmail.com>
2026-03-02 14:22:28 +0500

    UpdateProjectStructure

--- Chairperson/Services/ChairpersonServices.cs
+++ Chairperson/Services/ChairpersonServices.cs
@@ -1,6 +1,162 @@
-﻿namespace Project.Frontend.Chairperson.Services
+﻿using Project.Frontend.Model;
+using Project.Frontend.Model.DTOs;
+using System.Net.Http.Json;
+using System.Text.Json.Nodes;
+
+namespace Project.Frontend.Chairperson.Services
 {
     public class ChairpersonServices
     {
+
+        private readonly HttpClient httpClient;
+
+        public ChairpersonServices(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+
+        public async Task<List<Event>> GetPendingEvents(string memberId)
+        {
+            try
+            {
+                var respnse = await httpClient.GetAsync($"president/pendingEvents?memberId={memberId}");
+
+                if (!respnse.IsSuccessStatusCode)
+                    return new List<Event>();
+
+                var events = await respnse.Content.ReadFromJsonAsync<List<Event>>();
+
+                return events ?? new List<Event>();
+            }
+            catch
+            {
+                return new List<Event>();
+            }
+        }
+
+        public async Task<ResponseResult> DeleteEvent(Guid eventId)
+        {
+            try
+            {
+                var response = await httpClient.DeleteAsync($"president/deleteEvent/{eventId}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var resString = await response.Content.ReadAsStringAsync();
+                    var jsonNode = JsonNode.Parse(resString);
+                    var error = jsonNode?["errors"]?.ToString() ?? string.Empty;
+
+                    return new ResponseResult() { Success = false, Error = error };
+                }
+                return new ResponseResult() { Success = true };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult() { Success = false, Error = ex.Message };
+            }
+        }
+
+        public async Task<ResponseResult> UpdateEvent(UpdateEventDto updateEventDto)
+        {
+            try
+            {
+                var response = await httpClient.PutAsJsonAsync("president/updateEvent", updateEventDto);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var resString = await response.Content.ReadAsStringAsync();
+                    var jsonNode = JsonNode.Parse(resString);
+                    var error = jsonNode?["errors"]?.ToString() ?? string.Empty;
+
+                    return new ResponseResult() { Success = false, Error = error };
+                }
+
+                return new ResponseResult() { Success = true };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult() { Success = false, Error = ex.Message };
+            }
+        }
+
+        public async Task<Event?> GetEventById(Guid societyId)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"president/getEventById?eventId={societyId}");
+
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                else
+                {
+                    var @event = await response.Content.ReadFromJsonAsync<Event>();
+                    return @event;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<PendingEventRequisitionsDto>> GetPendingRequisitions(Guid id)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"ChairPerson/getPendingRequisitions?memberId={id}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new List<PendingEventRequisitionsDto>();
+                }
+
+                var pendingRequisitions = await response.Content.ReadFromJsonAsync<List<PendingEventRequisitionsDto>>();
+                return pendingRequisitions!;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<PendingEventRequisitionsDto>();
+            }
+        }
+
+        public async Task<ResponseResult> CreateRequisition(SendEventRequisitionDto requisitionDto)
+        {
+            try
+            {
+                var response = await httpClient.PostAsJsonAsync("chairperson/createRequisition", requisitionDto);
+                if (!response.IsSuccessStatusCode)
+                {
+                    var resString = await response.Content.ReadAsStringAsync();
+                    var jsonNode = JsonNode.Parse(resString);
+                    var error = jsonNode?["errors"]?.ToString() ?? string.Empty;
+
+                    return new ResponseResult() { Success = false, Error = error };
+                }
+
+                return new ResponseResult() { Success = true };
+
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult() { Success = false, Error = ex.Message };
+            }
+        }
+
+        public async Task<ChairpersonDetailForRequisitionFormDto?> GetDetailsForRequisition(Guid memberId)
+        {
+            var response = await httpClient.GetAsync($"ChairPerson/detailsForRequisition?id={memberId}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var memberDetails = await response.Content.ReadFromJsonAsync<ChairpersonDetailForRequisitionFormDto>();
+
+            return memberDetails;
+        }
     }
 }
